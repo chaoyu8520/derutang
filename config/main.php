0a7c68c6ffe5acd429be14d192f8c54e6792cbc2
chaoyu252 <734775208@qq.com>
2017-01-21 18:38:21 +0800

    撒旦法

--- config/main.php
+++ config/main.php
@@ -65,10 +65,10 @@ $config = [
     'params' => $params,
 ];
 
-if (YII_ENV_TEST) {
+//if (YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
   //  $config['bootstrap'][] = 'debug';
 //    $config['modules']['debug'] = 'yii\debug\Module';
-}
+//}
 
 return $config;
--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -5,10 +5,18 @@ use app\models\Ceshi;
 use yii\web\Controller;
 class IndexController extends Controller{
     public function actionIndex(){
+//
+//        exit;
         $ceshi = Ceshi::find()->asArray()->all();
         echo "数据库数据：<pre>";
         print_r($ceshi);
         echo "</pre>";
         phpinfo();
+    }
+
+    public function actionInfo()
+    {
+        phpinfo();
+        exit;
     }
 }
